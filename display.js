7571ddcf3c6b3f6dd2ff203e7e0da901a473297c
a262537412640768744 <358685701@qq.com>
2024-08-10 15:54:34 +0800

    v0.0

---
+++ display.js
@@ -0,0 +1,35 @@
+function display(value, a = true) {
+    if (value.lt(1) && value.gt(0.0001)) {
+        return (value.sign * value.mag).toFixed(2)
+    }
+    if (a) {
+        value = value.floor()
+    } else {
+        value = value.round()
+    }
+    if (value.layer === 0) {
+        if (value.mag < 1e9 && value.mag > 1e-4 || value.mag === 0) {
+            return (value.sign * value.mag).toFixed(0)
+        }
+        return value.m.toFixed(2) + "e" + value.e
+    } else if (value.layer === 1) {
+        if (value.mag < 1e6) {
+            return value.m.toFixed(2) + "e" + value.e
+        }
+        return "e" + display(value.log10())
+    } else {
+        return "e" + display(value.log10())
+    }
+}
+
+let center0 = document.querySelectorAll('.center0').item(0)
+let center1 = document.querySelectorAll('.center1').item(0)
+
+function update() {
+    UpdateA0n()
+    center0.innerHTML = DisplayA0()
+    center1.innerHTML = DisplayA01_8()
+    setTimeout(update, 10)
+}
+
+update()
